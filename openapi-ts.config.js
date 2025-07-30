0cb358c9ee6c0e4abaddf20890b864e17912aed8
mattbrailsford <me@mattbrailsford.com>
2025-07-30 09:30:12 +0100

    Initial commit + client generation

---
+++ openapi-ts.config.js
@@ -0,0 +1,25 @@
+﻿import { defineConfig } from "@hey-api/openapi-ts";
+
+export default defineConfig({
+    debug: true,
+    input: "https://localhost:44322/umbraco/swagger/commerce-management/swagger.json",
+    output: {
+        path: "src/api",
+    },
+    plugins: [
+        {
+            name: "@hey-api/client-fetch",
+            exportFromIndex: true,
+            throwOnError: true,
+        },
+        {
+            name: "@hey-api/typescript",
+            enums: "typescript",
+        },
+        {
+            name: "@hey-api/sdk",
+            asClass: true,
+        },
+        'zod'
+    ],
+});
