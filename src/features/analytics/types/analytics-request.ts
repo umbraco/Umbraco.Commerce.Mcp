3f07fff730cc2e5095cc263a6dc6d35e0193cf83
mattbrailsford <me@mattbrailsford.com>
2025-08-09 19:03:00 +0100

    Provide hint of for expected date format

--- src/features/analytics/types/analytics-request.ts
+++ src/features/analytics/types/analytics-request.ts
@@ -2,10 +2,10 @@
 import { storeIdOrAliasRequestSchema } from "../../../common/types/store.js";
 
 export const analyticsRequestSchema = storeIdOrAliasRequestSchema.extend({
-    from: z.string().datetime().describe('Start date for the analytics data range'),
-    to: z.string().datetime().describe('End date for the analytics data range'),
-    compareFrom: z.string().datetime().optional().nullable().describe('Start date for the comparison period'),
-    compareTo: z.string().datetime().optional().nullable().describe('End date for the comparison period'),
+    from: z.string().datetime().describe('Start date for the analytics data range (yyyy-MM-ddTHH:mm:ssZ)'),
+    to: z.string().datetime().describe('End date for the analytics data range (yyyy-MM-ddTHH:mm:ssZ)'),
+    compareFrom: z.string().datetime().optional().describe('Start date for the comparison period (yyyy-MM-ddTHH:mm:ssZ)'),
+    compareTo: z.string().datetime().optional().describe('End date for the comparison period (yyyy-MM-ddTHH:mm:ssZ)'),
     localTimezoneOffset: z.number().describe('Local timezone offset in minutes'),
     localCulture: z.string().describe('Local culture identifier for formatting dates and numbers'),
 });
